9260c5cc950e4e1ce14cc29982ed6bc93e916f96
Tim Gundmann <tim@gundmann.dk>
2014-12-23 19:34:04 +0100

    Print debug info

--- src/main/java/dk/gundmann/jenkins/cddbplugin/database/ResolveSqlScript.java
+++ src/main/java/dk/gundmann/jenkins/cddbplugin/database/ResolveSqlScript.java
@@ -94,6 +94,8 @@ public class ResolveSqlScript implements Command {
 				File file = new File(dir + "/" + fileName);
 				try {
 					BasicFileAttributes attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+					System.out.println(attr.creationTime());
+					System.out.println(file.toPath());
 					if (attr.creationTime().to(TimeUnit.MILLISECONDS) > lastDate.getTime()) {
 						updateFiles.add(file);
 					}
